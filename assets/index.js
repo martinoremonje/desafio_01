75461d425f37bc04739fc62c7af3e85b9cfeae67
martinoremonje <m.monjeorellana@gmail.com>
2024-03-06 17:25:26 -0300

    subir programa

---
+++ assets/index.js
@@ -0,0 +1,53 @@
+let numberOfGames = parseInt(prompt("ingrese numero de juegos"));
+const choices = ["tijera", "papel", "piedra"]
+var wins = 0;
+var ties = 0;
+var lost = 0;
+let winsSpan = document.getElementById("wins");
+let tiesSpan = document.getElementById("ties");
+let losesSpan = document.getElementById("loses");
+
+for (let i = 0; i < numberOfGames; i++){
+    let randomChoice = choices[Math.floor(Math.random() * 3)]
+    let choice = prompt("ingrese tijera, papel, piedra en minusculas");
+    switch(choice){
+        case "tijera": if(randomChoice == "tijera"){
+            alert("El programa saco Tijera, Empate");
+            ties += 1;
+        } else if(randomChoice == "papel"){
+            alert("El programa saco Papel, Ganaste!")
+            wins += 1;
+        } else {
+            alert("El programa saco Piedra, Perdiste")
+            lost += 1;
+        }
+        break;
+        case "papel": if(randomChoice == "tijera"){
+            alert("El programa saco Tijera, Perdiste");
+            lost += 1;
+        } else if(randomChoice == "papel"){
+            alert("El programa saco Papel, Empate")
+            ties += 1;
+        } else {
+            alert("El programa saco Piedra, Ganaste")
+            wins += 1;
+        }
+        break;
+        case "piedra": if(randomChoice == "tijera"){
+            alert("El programa saco Tijera, Ganaste");
+            wins += 1;
+        } else if(randomChoice == "papel"){
+            alert("El programa saco Papel, Perdiste")
+            lost += 1;
+        } else {
+            alert("El programa saco Piedra, Empate")
+            ties += 1;
+        }
+        break;  
+        default: alert("Escriba una de las tres opciones, con minusculas. Pierdes un juego.")
+        break;
+    }
+    winsSpan.innerHTML = wins;
+    tiesSpan.innerHTML = ties;
+    losesSpan.innerHTML = lost
+}
